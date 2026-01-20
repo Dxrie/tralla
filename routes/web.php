b5cc7aba562d1061e9e0c89ba4f39b367a866c29
Dxrie <colinedric04@gmail.com>
2026-01-20 10:11:32 +0700

    sigma

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -45,6 +46,3 @@ Route::get('/todos', [TodoController::class, 'create']);
 Route::get('/todos/{todo}/edit', [TodoController::class, 'edit']);
 Route::put('/todos/{todo}', [TodoController::class, 'update']);
 Route::delete('/todos/{todo}', [TodoController::class, 'destroy']);
-
-
-
