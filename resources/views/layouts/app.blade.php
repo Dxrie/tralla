10e1447febe46d8e41f7f4f89f5277bc4cc3b96b
Dxrie <101846302+Dxrie@users.noreply.github.com>
2026-01-22 16:35:37 +0700

    Declane/implement profile (#22)
    * Nav Revision
    
    * Rework Git Conflicts #2
    
    * Rework Git Conflicts #2
    
    * Rework Git Conflicts #2
    
    * yay
    
    * Conflict Remerge
    
    * Profile & Navigation Done
    
    ---------
    
    Co-authored-by: imdcln <declanecun@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
