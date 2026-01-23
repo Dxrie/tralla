662cfccbfa183fa1025c3951f0ff5a9b1ec3d48d
Declane <declanecun@gmail.com>
2026-01-23 13:30:31 +0700

    Jose (#29)
    * merge main to jose (#6)
    
    * feat: activities database migration (#1)
    
    * Dxrie/database migration (#2)
    
    * feat: activities database migration
    
    * add default value for role field
    
    * Declane (#5)
    
    * Day 1
    
    * Login & Register
    
    * yes (#4)
    
    * feat: activities database migration (#1)
    
    * Dxrie/database migration (#2)
    
    * feat: activities database migration
    
    * add default value for role field
    
    * Revision
    
    ---------
    
    Co-authored-by: imdcln <declanecun@gmail.com>
    
    * install sanctum for auth
    
    ---------
    
    Co-authored-by: imdcln <declanecun@gmail.com>
    
    * merge jose (#7)
    
    * feat: activities database migration (#1)
    
    * Dxrie/database migration (#2)
    
    * feat: activities database migration
    
    * add default value for role field
    
    * Declane (#5)
    
    * Day 1
    
    * Login & Register
    
    * yes (#4)
    
    * feat: activities database migration (#1)
    
    * Dxrie/database migration (#2)
    
    * feat: activities database migration
    
    * add default value for role field
    
    * Revision
    
    ---------
    
    Co-authored-by: imdcln <declanecun@gmail.com>
    
    * install sanctum for auth
    
    ---------
    
    Co-authored-by: imdcln <declanecun@gmail.com>
    
    * menambahkan design
    
    * tambah
    
    * coba
    
    * walalu
    
    * update
    
    * lagi
    
    * peminjaman
    
    * perbaikan
    
    ---------
    
    Co-authored-by: Dxrie <101846302+Dxrie@users.noreply.github.com>
    Co-authored-by: Keitatoo <josekeitaro2008@gmail.com>

--- app/Http/Controllers/Dashboard/PeminjamanController.php
+++ app/Http/Controllers/Dashboard/PeminjamanController.php
@@ -3,12 +3,80 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Borrow;
 use Illuminate\Http\Request;
 
 class PeminjamanController extends Controller
 {
     public function index()
     {
-        return view('users.peminjaman.index');
+        $borrows = Borrow::all();
+        return view('users.peminjaman.index', compact('borrows'));
+    }
+
+    public function create()
+    {
+        return view('users.peminjaman.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_barang' => 'required|string|max:255',
+            'divisi' => 'required',
+            'foto_barang' => 'required|image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+
+        $foto = $request->file('foto_barang')->store('barang', 'public');
+
+        Borrow::create([
+            'nama_barang' => $request->nama_barang,
+            'divisi' => $request->divisi,
+            'foto_barang' => $foto,
+        ]);
+
+        return redirect()
+            ->route('peminjaman.index')
+            ->with('success', 'Barang berhasil dipinjam');
+    }
+
+    public function edit($id)
+    {
+        $borrow = Borrow::findOrFail($id);
+        return view('users.peminjaman.edit', compact('borrow'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $borrow = Borrow::findOrFail($id);
+
+        $request->validate([
+            'nama_barang' => 'required|string|max:255',
+            'divisi' => 'required',
+            'foto_barang' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+
+        if ($request->hasFile('foto_barang')) {
+            $foto = $request->file('foto_barang')->store('barang', 'public');
+            $borrow->foto_barang = $foto;
+        }
+
+        $borrow->nama_barang = $request->nama_barang;
+        $borrow->divisi = $request->divisi;
+        $borrow->save();
+
+        return redirect()
+            ->route('peminjaman.index')
+            ->with('success', 'Data peminjaman berhasil diperbarui');
+    }
+    
+    public function destroy($id)
+    {
+        $borrow = Borrow::findOrFail($id);
+        $borrow->delete();
+
+        return redirect()
+            ->route('peminjaman.index')
+            ->with('success', 'Data berhasil dihapus');
     }
 }
